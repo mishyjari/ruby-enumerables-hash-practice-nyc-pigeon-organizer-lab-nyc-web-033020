e12ba8c885e9b18a1ab5ca79b7a12441087366bd
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-10 15:38:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
     value.each_pair{ | pair | 
      # p pair 
       pair[1].each{ | name |
-        memo[name] = { key => pair[0].to_s }
+        memo[name] = key => pair[0].to_s }
       }
     }
     pp memo
