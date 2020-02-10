e807c265c32d4b9f8a573e5436eee70f9c821d2f
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-10 16:46:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,30 +1,22 @@
 def nyc_pigeon_organizer(data)
   output = {}
+
+  # First pass using the redcuce method. Key represents traits (color,gender,lives) where value is a nested hash
   data.reduce({}) do | memo, (trait,value) |
-#    p memo
-#    p trait
-
-#  output[trait] = nil
-
-   value.reduce({}) do | item, (key, names) |
-
-      names.each{| name | 
-        if !output[name] 
-          output[name] = { trait => [key.to_s] } 
-        elsif !output[name][trait]
-          output[name][trait] = [key.to_s] 
-        else 
-          output[name][trait] << key.to_s
-        end 
-        }  
-      
-    end
-    
-    
-    #memo.each_pair{|pair| output[pair[0]] = pair[1]}
   
-#  pp output    
-  end
-pp output
+    # Pass reduce on the nested hash to extract an array of names and the value for each trait
+    value.reduce({}) do | item, (key, names) |
 
+      # Iterate over the names array and add data to output
+      names.each{| name | 
+        if !output[name] # Name doesn't exist, add new hash
+          output[name] = { trait => [key.to_s] } 
+        elsif !output[name][trait] # Name exists, but not trait, add new nested hash
+          output[name][trait] = [key.to_s] 
+        else # Trait already exists, but there is more data to add. Push that into the value array
+          output[name][trait] << key.to_s
+        end }  
+    end
+  end
+  output
 end
