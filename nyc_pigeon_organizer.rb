b6dbe27183461b24fea0c9698dcfcadb7c82198e
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-10 15:54:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
     p trait
     value.reduce({}) do | item, (key, names) |
       p key
-      names.each{|name| memo[name] = key}
+      names.
     end
   end
   pp memo
