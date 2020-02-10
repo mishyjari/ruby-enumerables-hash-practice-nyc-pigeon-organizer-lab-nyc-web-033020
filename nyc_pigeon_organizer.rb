3dec6747876ebc4b6ae0640fa088c4a990006381
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-10 15:41:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,7 @@ def nyc_pigeon_organizer(data)
   data.reduce({}) do | memo, (key,value) |
     p memo
     p key
-    value.each_pair{ | pair | 
-      p pair 
-      pair[1].each{ | name |
-        memo[name] = key
-      }
+    memo[key] = value
     }
     pp memo
   end
